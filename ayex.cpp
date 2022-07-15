027c288572896084f780915490f88dd83a8be297
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-07-15 23:22:00 +0300

    C plus

---
+++ ayex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char const *argv[])
+{
+   std::cout << "Hello World" << std::endl;
+   
+  return 0;
+}
